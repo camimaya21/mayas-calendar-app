15f6e3300dad5d5f7bd3c2f46203a38f64f4be17
camimaya21 <camilamaya21@gmail.com>
2021-03-03 23:36:34 +0100

    fix: i18n config and react-query initialization

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import reportWebVitals from './reportWebVitals'
+import App from './App'
+import './utils/i18n'
+import './index.css'
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
