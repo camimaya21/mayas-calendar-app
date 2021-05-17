180423be497ba4fa3f006532118bb38bbbbae62b
camimaya21 <camilamaya21@gmail.com>
2021-05-17 21:08:20 +0200

    test: added subcategories mock

---
+++ src/__mocks__/subcategories.mock.ts
@@ -0,0 +1,121 @@
+export const MOCK_SUBCATEGORIES_EVBRITE_PAGE_1 = {
+  pagination: {
+    continuation: 'eyJwYWdlIjogMn0',
+    has_more_items: true,
+    object_count: 6,
+    page_count: 3,
+    page_number: 1,
+    page_size: 2
+  },
+  subcategories: [
+    {
+      id: '1001',
+      name: 'Startups & Small Business',
+      name_localized: 'Startups & Small Business',
+      parent_category: {
+        id: '101',
+        name: 'Business & Professional',
+        name_localized: 'Business & Professional',
+        resource_uri: 'https://www.mock.com/v3/categories/101/',
+        short_name: 'Business',
+        short_name_localized: 'Business'
+      },
+      resource_uri: 'https://www.mock.com/v3/subcategories/1001/'
+    },
+    {
+      id: '1002',
+      name: 'Finance',
+      name_localized: 'Finance',
+      parent_category: {
+        id: '101',
+        name: 'Business & Professional',
+        name_localized: 'Business & Professional',
+        resource_uri: 'https://www.mock.com/v3/categories/101/',
+        short_name: 'Business',
+        short_name_localized: 'Business'
+      },
+      resource_uri: 'https://www.mock.com/v3/subcategories/1002/'
+    }
+  ]
+}
+
+export const MOCK_SUBCATEGORIES_EVBRITE_PAGE_2 = {
+  pagination: {
+    continuation: 'eyJwYWdlIjogMn1',
+    has_more_items: true,
+    object_count: 6,
+    page_count: 3,
+    page_number: 2,
+    page_size: 2
+  },
+  subcategories: [
+    {
+      id: '1003',
+      name: 'Startups & Small Business',
+      name_localized: 'Startups & Small Business',
+      parent_category: {
+        id: '101',
+        name: 'Business & Professional',
+        name_localized: 'Business & Professional',
+        resource_uri: 'https://www.mock.com/v3/categories/101/',
+        short_name: 'Business',
+        short_name_localized: 'Business'
+      },
+      resource_uri: 'https://www.mock.com/v3/subcategories/1001/'
+    },
+    {
+      id: '1004',
+      name: 'Finance',
+      name_localized: 'Finance',
+      parent_category: {
+        id: '101',
+        name: 'Business & Professional',
+        name_localized: 'Business & Professional',
+        resource_uri: 'https://www.mock.com/v3/categories/101/',
+        short_name: 'Business',
+        short_name_localized: 'Business'
+      },
+      resource_uri: 'https://www.mock.com/v3/subcategories/1002/'
+    }
+  ]
+}
+
+export const MOCK_SUBCATEGORIES_EVBRITE_PAGE_3 = {
+  pagination: {
+    has_more_items: false,
+    object_count: 6,
+    page_count: 3,
+    page_number: 3,
+    page_size: 2
+  },
+  subcategories: [
+    {
+      id: '1005',
+      name: 'Startups & Small Business',
+      name_localized: 'Startups & Small Business',
+      parent_category: {
+        id: '101',
+        name: 'Business & Professional',
+        name_localized: 'Business & Professional',
+        resource_uri: 'https://www.mock.com/v3/categories/101/',
+        short_name: 'Business',
+        short_name_localized: 'Business'
+      },
+      resource_uri: 'https://www.mock.com/v3/subcategories/1001/'
+    },
+    {
+      id: '1006',
+      name: 'Finance',
+      name_localized: 'Finance',
+      parent_category: {
+        id: '101',
+        name: 'Business & Professional',
+        name_localized: 'Business & Professional',
+        resource_uri: 'https://www.mock.com/v3/categories/101/',
+        short_name: 'Business',
+        short_name_localized: 'Business'
+      },
+      resource_uri: 'https://www.mock.com/v3/subcategories/1002/'
+    }
+  ]
+}
