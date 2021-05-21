ff8af28292c45a1501fa4187fde1867003eca1e8
Camila Maya S <31632069+camimaya21@users.noreply.github.com>
2021-05-22 00:09:19 +0200

    add: toast notifications in query mutations (#17)

---
+++ src/styles/toastCustomStyles.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { ToastContainer } from 'react-toastify'
+
+export const StyledToastContainer = styled(ToastContainer)`
+  .Toastify__toast--error {
+    background-color: var(--error-toast);
+  }
+`
