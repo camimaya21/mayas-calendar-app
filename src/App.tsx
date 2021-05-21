e86f115c1712b27620bf2f5d05d34628df10bb41
camimaya21 <camilamaya21@gmail.com>
2021-05-22 00:05:26 +0200

    add: toast notifications in query mutations

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Router } from 'routes/Router'
-
+import 'react-toastify/dist/ReactToastify.min.css'
+import { StyledToastContainer } from 'styles/toastCustomStyles'
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <StyledToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={true}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Router />
     </BrowserRouter>
   )
